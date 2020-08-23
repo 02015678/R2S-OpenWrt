46562087ba60fb8ff95857d3e60f60a79bfc5578
02015678 <02015678@users.noreply.github.com>
2020-08-23 17:29:35 +0800

    Add luci-lib-fs for dependency requirememt
    Add luci-lib-fs for dependency requirememt of luci-app-filetransfer
    from https://github.com/coolsnowwolf/lede/

--- SCRIPTS/02_prepare_package.sh
+++ SCRIPTS/02_prepare_package.sh
@@ -174,6 +174,7 @@ svn co https://github.com/project-openwrt/openwrt/branches/master/package/lean/c
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/adbyby package/lean/coremark/adbyby
 # File Transfer
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-filetransfer  package/lean/luci-app-filetransfer
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-lib-fs package/lean/luci-lib-fs
 #访问控制
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-accesscontrol package/lean/luci-app-accesscontrol
 #AutoCore
