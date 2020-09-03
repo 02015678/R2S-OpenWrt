f623defb7379fc9ef1b4638ed2ccfa5cf37983b9
02015678 <02015678@users.noreply.github.com>
2020-09-03 12:11:55 +0800

    Add NPS内网穿透

--- SCRIPTS/02_prepare_package.sh
+++ SCRIPTS/02_prepare_package.sh
@@ -150,6 +150,9 @@ mkdir package/lean/coremark/patches
 wget -P package/lean/coremark/patches/ https://raw.githubusercontent.com/QiuSimons/Others/master/coremark.patch
 #迅雷快鸟
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-xlnetacc package/lean/luci-app-xlnetacc
+#NPS内网穿透
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/npc package/lean/npc
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-nps package/lean/luci-app-nps
 #DDNS
 rm -rf ./feeds/packages/net/ddns-scripts
 rm -rf ./feeds/luci/applications/luci-app-ddns
